e4b43487792ed697fda056bf02f5afbaed421980
kevin olson <acidjazz@gmail.com>
2024-05-01 18:15:02 -0500

    💚 asset grab test 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import * as exec from '@actions/exec'
 
 /**
  * The main function for the action.
@@ -20,8 +21,30 @@ export async function run(): Promise<void> {
     const asset = release.assets.find(a =>
       a.name.match(/vc_.*_linux_amd64.tar.gz/)
     )
-    console.log('our asset below')
-    console.log(asset)
+
+    if (!asset || !asset.browser_download_url) {
+      throw new Error('Unable to find the asset in the release.')
+    }
+
+    // Download the asset using wget
+    await exec.exec(
+      `wget -q -O ${asset.name} -H "Authorization: token ${pat}" ${asset.browser_download_url}`
+    )
+
+    // Extract the file
+    await exec.exec(`tar -zxvf ${asset.name}`)
+
+    // Execute ls -la and log the output
+    let output = ''
+    const options = {
+      listeners: {
+        stdout: (data: Buffer) => {
+          output += data.toString()
+        }
+      }
+    }
+    await exec.exec('ls -la', [], options)
+    console.log(output)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
