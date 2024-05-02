657712b4be1c90c8676fb8308524a3c7e871a8f1
kevin olson <acidjazz@gmail.com>
2024-05-01 23:17:23 -0500

    💚 asset grab test 20

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import * as exec from '@actions/exec'
+import * as fs from 'fs'
+import axios from 'axios'
 
 /**
  * The main function for the action.
@@ -14,29 +16,27 @@ export async function run(): Promise<void> {
 
     const { data: release } = await octokit.rest.repos.getLatestRelease({
       owner: 'vulncheck-oss',
-      repo: 'cli'
+      repo: 'cli',
     })
 
     // walk through release.assets and look for  vc_*_linux_amd64.tar.gz
     const asset = release.assets.find(a =>
-      a.name.match(/vc_.*_linux_amd64.tar.gz/)
+      a.name.match(/vc_.*_linux_amd64.tar.gz/),
     )
 
     if (!asset || !asset.browser_download_url) {
       throw new Error('Unable to find the asset in the release.')
     }
 
-    console.log(asset)
+    const response = await axios.get(asset.url, {
+      responseType: 'arraybuffer',
+      headers: {
+        Accept: '*/*',
+        Authorization: `token ${pat}`,
+      },
+    })
 
-    /*
-    await exec.exec(
-      `wget --auth-no-challenge --header 'Authorization: token ${pat}' --header='Accept:application/octet-stream' https://api.github.com/repos/vulncheck-oss/cli/releases/assets/${asset.id} -O ${asset.name}`
-    )
-
-    await exec.exec(`
-      curl -L -H "Accept: application/octet-stream"  -H "Authorization: Bearer ${pat}" -H "X-GitHub-Api-Version: 2022-11-28" -o ${asset.name} https://api.github.com/repos/vulncheck-oss/cli/releases/assets/${asset.id}
-    `)
-    */
+    fs.writeFileSync(asset.name, response.data)
 
     // Execute ls -la and log the output
     let output = ''
@@ -44,8 +44,8 @@ export async function run(): Promise<void> {
       listeners: {
         stdout: (data: Buffer) => {
           output += data.toString()
-        }
-      }
+        },
+      },
     }
     await exec.exec('ls -la', [], options)
     console.log(output)
