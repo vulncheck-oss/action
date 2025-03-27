f068998baee3a2cbb647a17f5df55ddd55d2c412
kevin olson <acidjazz@gmail.com>
2025-03-27 18:28:50 -0500

    🐛 updates the cve link

--- src/table.ts
+++ src/table.ts
@@ -72,7 +72,7 @@ export function rows(
           { value: vuln.version },
           {
             value: vuln.cve,
-            link: `https://vulncheck.com/browse/cve/${vuln.cve}`,
+            link: `https://vulncheck.com/cve/${vuln.cve}`,
           },
           {
             value: vuln.in_kev
